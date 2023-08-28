92efac41510c7781d874f0ac9ec52449628298d5
Jim <jim@advancebass.com>
2023-08-28 13:02:28 -0700

    add cool beans

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res)=>{
     res.send('Hello, World');
 });
 
+app.get('/test', (req, res)=>{
+    res.send('Cool beans 🫘');
+});
+
 app.listen(PORT, ()=>{
     console.log(`App running on port ${PORT}`);
 });
