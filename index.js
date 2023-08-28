3fe44f5b350c8a3c0068b9b04d80149d1b668127
Jim <jim@advancebass.com>
2023-08-28 13:00:11 -0700

    add home route

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5050;
 
-app.get('Hello, World');
+app.get('/', (req, res)=>{
+    res.send('Hello, World');
+});
 
 app.listen(PORT, ()=>{
     console.log(`App running on port ${PORT}`);
